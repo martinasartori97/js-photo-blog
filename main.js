44deece22ef0f4f0550039767f879860f054c308
martinasartori97 <martinasartori9@gmail.com>
2024-10-18 14:40:04 +0200

    edit js

--- main.js
+++ main.js
@@ -46,8 +46,14 @@ console.log(card);
 
 //ciclo forEach
 card.forEach(element => {
-    console.log(card);
+    console.log(element);
 
 });
+
+axios
+.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+.then((response)=>{
+console.log(response.data)
+})
 
 
